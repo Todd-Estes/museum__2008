103e6f1a6a0bd2f1eccf6f32b26f2844df8ecb60
todd-estes <elestes@gmail.com>
2020-09-10 09:41:45 -0600

    Add add_exhibit method test and exhibit attribute test

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -2,13 +2,29 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/exhibit'
 require './lib/patron'
-require '.lib/museum'
+require './lib/museum'
 
 
 class MuseumTest < Minitest::Test
   def test_it_exists_and_has_attributes
     dmns = Museum.new("Denver Museum of Nature and Science")
 
+    assert_instance_of Museum, dmns
+
     assert_equal "Denver Museum of Nature and Science", dmns.name
 
   end
+  def test_it_has_exhibits
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    assert_equal [], dmns.exhibits
+
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    # require "pry"; binding.pry
+    assert_equal [gems_and_minerals, dead_sea_scrolls, imax], dmns.exhibits
+  end
+end
