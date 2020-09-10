7d5c68459347f92d34ba089351037ab7688dbd77
todd-estes <elestes@gmail.com>
2020-09-10 10:23:57 -0600

    Add recommend_exhibits method test

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -16,6 +16,7 @@ class MuseumTest < Minitest::Test
   end
   def test_it_has_exhibits
     dmns = Museum.new("Denver Museum of Nature and Science")
+
     assert_equal [], dmns.exhibits
 
     gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
@@ -24,7 +25,35 @@ class MuseumTest < Minitest::Test
     dmns.add_exhibit(gems_and_minerals)
     dmns.add_exhibit(dead_sea_scrolls)
     dmns.add_exhibit(imax)
-    # require "pry"; binding.pry
+
     assert_equal [gems_and_minerals, dead_sea_scrolls, imax], dmns.exhibits
+  end
+
+  def test_it_gives_recommendations
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    # require "pry"; binding.pry
+
+
+    patron_1 = Patron.new("Bob", 20)
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+    # require "pry"; binding.pry
+
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("IMAX")
+
+    dmns.recommend_exhibits(patron_1)
+    dmns.recommend_exhibits(patron_2)
+
+    assert_equal [gems_and_minerals, dead_sea_scrolls], dmns.recommend_exhibits(patron_1)
+    assert_equal [imax],dmns.recommend_exhibits(patron_2)
+
+
   end
 end
