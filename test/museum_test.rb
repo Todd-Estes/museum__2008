bd8e69b26e1ef3e3a962a8c9c534d0531f484182
todd-estes <elestes@gmail.com>
2020-09-10 09:28:31 -0600

    Add instance and attribute  test for museum class

---
+++ test/museum_test.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/exhibit'
+require './lib/patron'
+require '.lib/museum'
+
+
+class MuseumTest < Minitest::Test
+  def test_it_exists_and_has_attributes
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    assert_equal "Denver Museum of Nature and Science", dmns.name
+
+  end
