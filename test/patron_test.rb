d84b371b53d9e5f2ba0a14a1dbad5c150a2886ff
todd-estes <elestes@gmail.com>
2020-09-10 09:24:47 -0600

    Correct patron_test

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -22,6 +22,7 @@ class PatronTest < Minitest::Test
     patron_1.add_interest("Dead Sea Scrolls")
     patron_1.add_interest("Gems and Minerals")
 
-    ["Dead Sea Scrolls", "Gems and Minerals"], patron_1.interests
+  assert_equal ["Dead Sea Scrolls", "Gems and Minerals"], patron_1.interests
+  end
 
 end
